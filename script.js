514169860bcaa0027274cbe30e54d142481aab09
Yaohao <106961008+xiGHK@users.noreply.github.com>
2023-01-16 22:02:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function scrollup() {
+    document.querySelector(".scrollmarking-0").scrollIntoView()
+}
+
+function scrollsoftwareeng() {
+    document.querySelector(".scrollmarking-1").scrollIntoView()
+}
+
+function scrollhardwareeng() {
+    document.querySelector(".scrollmarking-2").scrollIntoView()
+    loadLangProf()
+}
+
+function scrollwebdeveloper() {
+    document.querySelector(".scrollmarking-3").scrollIntoView()
+}
+
+function scrollnetworkengineer() {
+    document.querySelector(".scrollmarking-4").scrollIntoView()
+}
+
+function scrolldatascientist() {
+    document.querySelector(".scrollmarking-5").scrollIntoView()
+}
+
+function scrollsecurityengineer() {
+    document.querySelector(".scrollmarking-6").scrollIntoView()
+}
